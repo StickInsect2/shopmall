cdc4c26abf97896bbc0ffd1a0890fc7c17a70835
StickInsect2 <1337704489@qq.com>
2019-12-16 20:27:37 +0800

    初始化thinkphp5配置

---
+++ application/route.php
@@ -0,0 +1,17 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+
+//动态路由：
+use think\Route;
+
+//Route::rule('hello','sample/Test/hello','get|post');
+Route::post('hello/:id','sample/Test/hello');
