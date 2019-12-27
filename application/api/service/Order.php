582bd5bc01d8667ae3f88f38267eb367f29067ba
StickInsect2 <1337704489@qq.com>
2019-12-27 12:54:26 +0800

    订单创建并写入数据库

--- application/api/service/Order.php
+++ application/api/service/Order.php
@@ -2,10 +2,13 @@
 
 namespace app\api\service;
 
+use app\api\model\OrderProduct;
 use app\api\model\Product;
 use app\api\model\UserAddress;
 use app\lib\exception\OrderException;
 use app\lib\exception\UserException;
+use app\api\model\Order as OrderModel;
+use think\Exception;
 
 class Order {
 
@@ -32,9 +35,45 @@ class Order {
         }
 
         //第六步 开始创建订单
+        //订单的快照
         $orderSnap = $this->snapOrder($status);
 
     }
+
+    //第九步 ，生成订单并存入数据库
+    private function createOrder($snap) {
+        try {
+            //首先生成订单号
+            $orderNo = $this->makeOrderNo();
+            //实例化一个order模型对象并写入数据库
+            $order = new OrderModel();
+            $order->user_id = $this->uid;
+            $order->order_no = $orderNo;
+            $order->total_price = $snap['orderPrice'];
+            $order->total_count = $snap['totalCount'];
+            $order->snap_img = $snap['snapImg'];
+            $order->snap_name = $snap['snapName'];
+            $order->snap_address = $snap['snapAddress'];
+            $order->snap_items = json_encode($snap['pStatus']);
+            $order->save();
+
+            $orderID = $order->id;
+            $create_time = $order->create_time;
+            foreach ($this->oProducts as &$p) {
+                $p['order_id'] = $orderID;
+            }
+            $orderProduct = new OrderProduct();
+            $orderProduct->saveAll($this->oProducts);
+            return [
+                'order_no' => $orderNo,
+                'order_id' => $orderID,
+                'create_time' => $create_time
+            ];
+        } catch (Exception $e) {
+            throw  $e;
+        }
+    }
+
 
     //第八步：用户的地址
     private function getUserAddress() {
@@ -70,7 +109,7 @@ class Order {
         if (count($this->products) > 1) {
             $snap['snapName'] .= '等';
         }
-        
+
         return $snap;
     }
 
@@ -138,5 +177,15 @@ class Order {
             ->visible(['id', 'price', 'stock', 'name', 'main_img_url'])
             ->toArray();
         return $products;
+    }
+
+    //生成订单号
+    public static function makeOrderNo() {
+        $yCode = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
+        $orderSn =
+            $yCode[intval(date('Y')) - 2017] . strtoupper(dechex(date('m'))) . date(
+                'd') . substr(time(), -5) . substr(microtime(), 2, 5) . sprintf(
+                '%02d', rand(0, 99));
+        return $orderSn;
     }
 }
